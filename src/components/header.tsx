c37ef26e5d1b2d2b8f4fdb7d74425baab059630b
Prashant <prashantco111@gmail.com>
2025-03-08 23:23:49 +0530

    Added logout feature

---
+++ src/components/header.tsx
@@ -0,0 +1,23 @@
+import type { Component } from 'solid-js';
+import { signInWithPopup, GithubAuthProvider, signOut } from "firebase/auth";
+import { auth } from "@/utils/firebase"
+import { useNavigate } from '@solidjs/router';
+
+const Header: Component = () => {
+  const navigate = useNavigate()
+  const signOutUser = () => {
+    signOut(auth).then(() => {
+      navigate("/login")
+      // Sign-out successful.
+    }).catch((error) => {
+      console.error(error)
+      // An error happened.
+    });
+  }
+
+  return (
+    <button onClick={signOutUser} class="btn btn-error">Sign Out</button>
+  );
+};
+
+export default Header;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,17 @@
+import Header from '@/components/header';
+import { useAppState } from '@/state/auth';
 import type { Component } from 'solid-js';
 
 const HomePage: Component = () => {
+  const [appState, _] = useAppState();
+
   return (
-    <h1 class="text-3xl font-bold underline">
-      Hello jorld!
-    </h1>
+    <div>
+      <Header />
+      <h1 class="text-3xl font-bold underline">
+        {JSON.stringify(appState.user, null, 2) || "Not found"}
+      </h1>
+    </div>
   );
 };
 
