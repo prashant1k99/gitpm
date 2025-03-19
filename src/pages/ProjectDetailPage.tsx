4240c50afbe6afe778960f457ee1219eed1cfb62
Prashant <prashantco111@gmail.com>
2025-03-19 16:17:46 +0530

    Added listing or tasks

--- src/pages/ProjectDetailPage.tsx
+++ src/pages/ProjectDetailPage.tsx
@@ -1,9 +1,14 @@
+import DB from "@/db/organization";
 import { loadAllFieldsForProject } from "@/state/fields";
 import { loadItemsForProject } from "@/state/items";
-import { useEffect } from "react";
+import orgState from "@/state/organizations";
+import { useSignalEffect } from "@preact/signals-react";
+import { useLiveQuery } from 'dexie-react-hooks';
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export default function ProjectDetailPage() {
+  const [orgLogin, setOrgLogin] = useState<string>(orgState.value.activeOrg?.login || "")
   const { projectNumber } = useParams();
 
   useEffect(() => {
@@ -12,11 +17,26 @@ export default function ProjectDetailPage() {
     })
   }, [projectNumber])
 
+  useSignalEffect(() => {
+    if (orgState.value.activeOrg?.login)
+      setOrgLogin(orgState.value.activeOrg?.login)
+  })
+
+  const items = useLiveQuery(() => {
+    const db = DB.getDatabases(orgLogin)
+    return db.tasks.where("projectId").equals(Number(projectNumber)).toArray()
+  }, [orgLogin, projectNumber])
+
   return (
     <div>
       <h1>
         Project Detail Page: {projectNumber}
       </h1>
+      {items?.map((item) => {
+        return (
+          <p key={item.id}>{item.content.title}</p>
+        )
+      })}
     </div>
   )
 }
