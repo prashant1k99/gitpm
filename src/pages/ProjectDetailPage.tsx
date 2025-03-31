7fe4dbfbcf582012da128ddb3cad6964ad71cc3c
Prashant <prashantco111@gmail.com>
2025-03-31 18:53:00 +0530

    Added basic tab feature for project detail page

--- src/pages/ProjectDetailPage.tsx
+++ src/pages/ProjectDetailPage.tsx
@@ -5,70 +5,17 @@ import { loadItemsForProject } from "@/state/items";
 import orgState from "@/state/organizations";
 import { useSignalEffect } from "@preact/signals-react";
 import { useLiveQuery } from 'dexie-react-hooks';
-import { Activity, Calendar, CirclePlus, FolderKanban, FolderLock, SquareChevronDown } from "lucide-react";
+import { FolderKanban, FolderLock, Info, UsersRound, View } from "lucide-react";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { getRelativeTimeFromNow } from "@/lib/utils";
-import { ProjectStatus } from "@/types/projects";
-import { Badge } from "@/components/ui/badge";
-
-function getStatusRenderDetails(status: ProjectStatus) {
-  let value = {
-    text: "Unknown",
-    color: "#ffffff",
-  }
-  switch (status) {
-    case ProjectStatus.AT_RISK:
-      value = {
-        text: "At Risk",
-        color: "#eb8626",
-      }
-      break;
-    case ProjectStatus.COMPLETE:
-      value = {
-        text: "Complete",
-        color: "#b038f0",
-      }
-      break;
-    case ProjectStatus.INACTIVE:
-      value = {
-        text: "Inactive",
-        color: "#b0b0b0"
-      }
-      break;
-    case ProjectStatus.OFF_TRACK:
-      value = {
-        text: "Off Track",
-        color: "#f8412b"
-      }
-      break;
-    case ProjectStatus.ON_TRACK:
-      value = {
-        text: "On Track",
-        color: "#0cc829"
-      }
-      break;
-  }
-
-  return (
-    <Badge
-      className=" border border-accent-foreground bg-accent text-accent-foreground"
-      style={{
-        borderColor: value.color,
-        backgroundColor: `${value.color}20`, // 20 is hex for 12% opacity
-        color: value.color
-      }}
-    >
-      {value.text}
-    </Badge>
-  )
-}
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { ProjectStatusForDetailPage } from "@/components/project/status";
 
 export default function ProjectDetailPage() {
   const { projectNumber } = useParams();
+
   let db = DB.getDatabases(orgState.value.activeOrg?.login as string)
 
   useEffect(() => {
@@ -109,84 +56,46 @@ export default function ProjectDetailPage() {
           <h1 className="text-3xl">
             {project.title}
           </h1>
-          <span className="mt-6">
-            {project.shortDescription}
-          </span>
-          <Separator className="bg-accent my-4" />
-          Get Milestones for all the repositories connected
-          <br />
+          {project.shortDescription && (
+            <span className="mt-6">
+              {project.shortDescription}
+            </span>
+          )}
           {project.readme && (
-            <div
-              className="prose prose-sm sm:prose-base lg:prose-lg xl:prose-xl prose-h1:text-2xl dark:prose-invert prose-p:my-1 prose-headings:my-2 prose-ul:my-1 prose-ol:my-1 prose-li:my-0.5 prose-pre:my-1 prose-blockquote:my-1"
-            >
-              <ReactMarkdown
-                children={project.readme}
-                remarkPlugins={[remarkGfm]}
-              />
-            </div>
+            <>
+              <Separator className="bg-accent my-4" />
+              <div
+                className="prose prose-sm sm:prose-base lg:prose-lg xl:prose-xl prose-h1:text-2xl dark:prose-invert prose-p:my-1 prose-headings:my-2 prose-ul:my-1 prose-ol:my-1 prose-li:my-0.5 prose-pre:my-1 prose-blockquote:my-1"
+              >
+                <ReactMarkdown
+                  children={project.readme}
+                  remarkPlugins={[remarkGfm]}
+                />
+              </div>
+            </>
           )}
           <Separator className="bg-accent my-4" />
-          {project.statusUpdates.nodes.length > 0 && (
-            <div className="flex flex-col gap-2">
-              <div className="flex gap-2 items-center">
-                <Activity className="h-4 w-4" />
-                Status Updates:
-              </div>
-              <div className="flex flex-col gap-2">
-                {project.statusUpdates.nodes.map(update => (
-                  <div key={update.id} className="p-2 border border-border rounded-md">
-                    <div className="flex gap-2 items-center bg-background">
-                      <div className="flex gap-2 items-center">
-                        <Avatar className="h-8 w-8 rounded-lg">
-                          <AvatarImage src={update.creator.avatarUrl} alt={update.creator.login || "User Name"} />
-                          <AvatarFallback className="rounded-lg">Github User</AvatarFallback>
-                        </Avatar>
-                        <p>{update.creator.login}</p>
-                      </div>
-                      <div>
-                        updated {getRelativeTimeFromNow(update.updatedAt)}
-                      </div>
-                    </div>
-                    <Separator className="bg-accent my-2" />
-                    <div className="overflow-x-auto">
-                      <table className="table-auto w-fit">
-                        <tbody>
-                          {update.targetDate && (
-                            <tr>
-                              <td className="font-bold px-4 py-2 flex items-center gap-2">
-                                <Calendar className="w-4 h-4" />
-                                Target Date:
-                              </td>
-                              <td className="px-4 py-2">{update.targetDate}</td>
-                            </tr>
-                          )}
-                          {update.startDate && (
-                            <tr>
-                              <td className="font-bold px-4 py-2 flex items-center gap-2">
-                                <CirclePlus className="w-4 h-4" />
-                                Start Date:
-                              </td>
-                              <td className="px-4 py-2">{update.startDate}</td>
-                            </tr>
-                          )}
-                          {update.status && (
-                            <tr>
-                              <td className="font-bold px-4 py-2 flex items-center gap-2">
-                                <SquareChevronDown className="w-4 h-4" />
-                                Status:
-                              </td>
-                              <td className="px-4 py-2">{getStatusRenderDetails(update.status)}</td>
-                            </tr>
-                          )}
-                        </tbody>
-                      </table>
-                    </div>
-                    <div className="content" dangerouslySetInnerHTML={{ __html: update.bodyHTML }}></div>
-                  </div>
-                ))}
-              </div>
-            </div>
-          )}
+          <Tabs defaultValue="info" className="w-full">
+            <TabsList className="gap-8">
+              <TabsTrigger value="info">
+                <Info className="h-4 w-4" />
+                Info
+              </TabsTrigger>
+              <TabsTrigger value="views">
+                <View className="h-4 w-4" />
+                Views
+              </TabsTrigger>
+              <TabsTrigger value="teams">
+                <UsersRound className="h-4 w-4" />
+                Teams
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent value="info">
+              <ProjectStatusForDetailPage statusUpdates={project.statusUpdates.nodes} />
+            </TabsContent>
+            <TabsContent value="views">Views for project</TabsContent>
+            <TabsContent value="teams">Teams for Project</TabsContent>
+          </Tabs>
         </div>
       </div>
     </div>
